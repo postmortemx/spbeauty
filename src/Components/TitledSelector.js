64c8a7c27dd63b9590c78b951316d99f1ae5ec99
postmortemx <164642063+postmortemx@users.noreply.github.com>
2024-04-04 10:02:05 +0000

    Обновить TitledSelector

--- src/Components/TitledSelector.js
+++ src/Components/TitledSelector.js
@@ -7,23 +7,23 @@ const Select = styled.select`
     -webkit-appearance: none !important; 
     appearance: none !important;
     outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
+    border-radius: 24px;
+    border: solid #E2E8F0 1px;
+    background-color: #F8FAFC;
+    color: #0F172A;
     &:hover {
-        background-color: #D9E2EFB8;
+        opacity: 0.8;
     }
 `;
 
 function TitledSelector({title, options}) {
     const Title = title === undefined ? 
         (<div></div>) : 
-        (<div>{title}</div>);
+        (<div className="caption">{title}</div>);
     return (
         <div className='d-flex flex-column gap-2'>
             {Title}
-            <Select className='p-2'>
+            <Select className='px-3 py-2'>
                 {options.map(optionName => {
                     return (
                         <option key={optionName}>{optionName}</option>
@@ -33,5 +33,5 @@ function TitledSelector({title, options}) {
         </div>
     );
 }
-
+/*по заданию сказано уменьшить отступ между заголовком и инпутом, но тут и так стояло gap-2, что по сути тоже 8px*/
 export default TitledSelector;
